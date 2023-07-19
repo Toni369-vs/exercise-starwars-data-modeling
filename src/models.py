517a5a2cae8f44e8d22ecc93d874535b0c88948d
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-07-19 10:42:31 +0000

    Tarea Finalizada

--- src/models.py
+++ src/models.py
@@ -7,26 +7,63 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
+    
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250))
+    birth_year = Column(Integer)
+    gender = Column(String(250))
+    height = Column(Integer)
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+    favorites = relationship("Favorites", backref="characters", lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'planets'
+    
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    climate = Column(String(20))
+    population = Column(Integer)
+    orbital_period = Column(Integer)
+    rotation_period = Column(Integer)
+    diameter = Column(Integer)
+    favorites = relationship("Favorites", backref="planets", lazy=True)
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    model = Column(String(20))
+    cargo_capacity = Column(Integer)
+    consumables = Column(String(250))
+    passengers = Column(Integer)
+    crew = Column(String(250))
+    favorites = relationship("Favorites", backref="vehicles", lazy=True)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey("characters.id"), nullable=True)
+    planet_id = Column(Integer, ForeignKey("planets.id"), nullable=True)
+    vehicle_id = Column(Integer, ForeignKey("vehicles.id"), nullable=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+class Users(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    favorites = relationship("Favorites", backref="users", lazy=True)
+
+    def to_dict(self):
+        return {}
+
+
 render_er(Base, 'diagram.png')
